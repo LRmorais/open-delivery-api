4a3f43730adbc5ea50a6a9b8ba15f9e23afd38e5
Lucas Morais <lucas.morais23ti@gmail.com>
2025-03-28 16:28:20 -0300

    feat: atualizando urls

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -4,7 +4,7 @@ import v1Routes from './v1/index.route';
 
 const router = Router();
 
-router.use('/oauth', authRoute);
+router.use('/authentication/oauth', authRoute);
 router.use('/v1', v1Routes);
 
 export default router;
