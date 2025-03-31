f6fc61ec2ad4c7fe57bbabbc83c5f06b248a7cfb
Lucas Morais <lucas.morais23ti@gmail.com>
2025-03-31 09:28:55 -0300

    feat: comentando temporariamente o orderUrl

--- src/interfaces/EventPollingResponse.ts
+++ src/interfaces/EventPollingResponse.ts
@@ -4,7 +4,7 @@ export interface EventPollingResponse {
     eventId: string; // UUID do evento
     eventType: EventTypeEnum;
     orderId: number; // id interno da tabela orders
-    orderURL: string;
+    // orderURL: string;
     createdAt: string; // ISO string
     sourceAppId: string; // client_id da integração
     virtualBrand?: string;
