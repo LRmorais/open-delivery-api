0da5804762f3827095acc606db448cf3e538b48a
Lucas Morais <lucas.morais23ti@gmail.com>
2025-04-01 23:03:31 -0300

    feat: definindo rota de orders

--- src/routes/v1/index.route.ts
+++ src/routes/v1/index.route.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import eventsRoutes from './events.route';
+import ordersRoutes from './order.route';
 
 const router = Router();
 
 router.use('/', eventsRoutes);
+router.use('/orders',ordersRoutes)
 
 export default router;
